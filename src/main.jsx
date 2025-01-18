3c955b42826254169d0483b55b2ebf61f068637d
Shakil Ahmed <shakil200409@gmail.com>
2025-01-18 07:41:11 +0600

    Implemented react router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <div>This is home.</div>,
+  },
+  {
+    path: "/about",
+    element: <div>This is all About.</div>,
+  },
+  {
+    path: "/contact",
+    element: <div>Call me now!!</div>,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}></RouterProvider>
+  </StrictMode>
+);
